fe5c86dc35770fb8478e44d2d29a43725387c036
M ATIF ALI <matifaligoraya@gmail.com>
2024-11-23 22:59:32 +0500

    Product Update

--- wpbakery_elements/widgets/kek_products_display.php
+++ wpbakery_elements/widgets/kek_products_display.php
@@ -4,7 +4,7 @@ class WPBakeryKekProductDisplayElement {
         add_action('vc_before_init', array($this, 'register_elements'), 40);
         add_shortcode('kek_product_display', array($this, 'render_kek_product_display'), 40);
 
-        // Hook for handling autocomplete AJAX
+        // Hooks for handling autocomplete AJAX
         add_filter('vc_autocomplete_kek_product_display_categories_callback', array($this, 'get_categories_for_autocomplete'), 10, 1);
         add_filter('vc_autocomplete_kek_product_display_categories_render', array($this, 'render_category_for_autocomplete'), 10, 1);
     }
@@ -16,22 +16,11 @@ class WPBakeryKekProductDisplayElement {
             'category' => __('Kek Essentials', 'kek'),
             'params' => array(
                 array(
-                    'type' => 'autocomplete',
-                    'heading' => __('Categories', 'kek'),
-                    'param_name' => 'categories',
-                    'description' => __('Select WooCommerce product categories.', 'kek'),
-                    'settings' => array(
-                        'multiple' => true,
-                        'sortable' => true,
-                        'unique_values' => true,
-                    ),
-                ),
-                array(
                     'type' => 'textfield',
                     'heading' => __('Number of Products', 'kek'),
                     'param_name' => 'product_count',
                     'description' => __('Enter the number of products to display.', 'kek'),
-                    'value' => '4', // Default to 4 products
+                    'value' => '4',
                 ),
                 array(
                     'type' => 'dropdown',
@@ -47,46 +36,12 @@ class WPBakeryKekProductDisplayElement {
         ));
     }
 
-    public function get_categories_for_autocomplete($query) {
-        $categories = get_terms(array(
-            'taxonomy' => 'product_cat',
-            'name__like' => $query,
-            'hide_empty' => false,
-        ));
-
-        $suggestions = array();
-
-        foreach ($categories as $category) {
-            $suggestions[] = array(
-                'value' => $category->slug,
-                'label' => $category->name,
-            );
-        }
-
-        return $suggestions;
-    }
-
-    public function render_category_for_autocomplete($query) {
-        $term = get_term_by('slug', $query['value'], 'product_cat');
-
-        if ($term) {
-            return array(
-                'value' => $term->slug,
-                'label' => $term->name,
-            );
-        }
-
-        return false;
-    }
-
     public function render_kek_product_display($atts) {
         $atts = shortcode_atts(array(
-            'categories' => '',
             'product_count' => '4',
             'display_style' => 'grid',
         ), $atts);
 
-        $categories = explode(',', $atts['categories']);
         $product_count = (int)$atts['product_count'];
         $display_style = esc_attr($atts['display_style']);
 
@@ -94,13 +49,6 @@ class WPBakeryKekProductDisplayElement {
         $args = array(
             'post_type' => 'product',
             'posts_per_page' => $product_count,
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'product_cat',
-                    'field' => 'slug',
-                    'terms' => $categories,
-                ),
-            ),
         );
         $products = new WP_Query($args);
 
@@ -113,21 +61,45 @@ class WPBakeryKekProductDisplayElement {
         <div class="kek-product-display <?php echo $display_style; ?>">
             <div class="row">
                 <?php while ($products->have_posts()): $products->the_post(); ?>
+                    <?php 
+                        $product = wc_get_product(get_the_ID());
+                        $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                        $price = $product->get_price_html();
+                        $description = wp_trim_words(get_the_excerpt(), 20, '...');
+                    ?>
                     <div class="col-md-<?php echo ($display_style === 'grid') ? '3' : '12'; ?> product-item">
-                        <div class="card">
-                            <a href="<?php the_permalink(); ?>" class="product-image">
-                                <?php the_post_thumbnail('medium', array('class' => 'img-fluid')); ?>
-                            </a>
+                        <div class="card shadow kek-info-box">
+                            <?php if ($image_url): ?>
+                                <img src="<?php echo esc_url($image_url); ?>" alt="<?php the_title(); ?>" class="card-img-top">
+                            <?php endif; ?>
                             <div class="card-body">
-                                <h5 class="product-title">
-                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                </h5>
-                                <div class="product-price">
-                                    <?php echo wc_get_product(get_the_ID())->get_price_html(); ?>
-                                </div>
-                                <a href="<?php the_permalink(); ?>" class="btn btn-primary mt-3">
-                                    <?php _e('View Product', 'kek'); ?>
+                                <h3 class="ls-0 fw-bold mb-3"><?php the_title(); ?></h3>
+                                <p class="mb-5 text-black-50"><?php echo $description; ?></p>
+                                <a href="<?php the_permalink(); ?>" class="btn w-100 text-white bg-color rounded-3 p-3 fw-bold animation-cloud-btn">                    
+                                    <span class="cloud-button-content">
+                                        <i class="fab fa-youtube cloud-button-icon mx-2"></i> 
+                                        Buy Now
+                                    </span>
+                                    <span class="animation-cloud-btn-inner">
+                                        <span class="animation-cloud-parts">
+                                            <span class="animation-cloud-part"></span>
+                                            <span class="animation-cloud-part"></span>
+                                            <span class="animation-cloud-part"></span>
+                                            <span class="animation-cloud-part"></span>
+                                        </span>
+                                    </span>
                                 </a>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <span>Starting at <b class="text-kek"><?php echo $price; ?></b></span>
+                                    </div>
+                                    <div class="col-md-6 stars">
+                                        <?php for ($i = 0; $i < 5; $i++) : ?>
+                                            <i class="fa fa-star float-end text-kek"></i>
+                                        <?php endfor; ?>
+                                    </div>
+                                </div>    
                             </div>
                         </div>
                     </div>
