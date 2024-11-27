d8504101d75eb4d86db1d8d3a073c10e0b4f0607
M ATIF ALI <matifaligoraya@gmail.com>
2024-11-28 04:01:59 +0500

    Atif - Video Card

--- wpbakery_elements/widgets/kek-video-card.php
+++ wpbakery_elements/widgets/kek-video-card.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 class WPBakeryKekVideoCard {
     public function __construct() {
         add_action('vc_before_init', array($this, 'register_elements'), 40);
@@ -23,10 +23,23 @@ class WPBakeryKekVideoCard {
                     'description' => __('Select the platform for this video card.', 'kek'),
                 ),
                 array(
+                    'type' => 'checkbox',
+                    'heading' => __('Show Floating Icons', 'kek'),
+                    'param_name' => 'show_icons',
+                    'value' => array(__('Yes', 'kek') => 'yes'),
+                    'description' => __('Check to show floating icons on the card.', 'kek'),
+                ),
+                array(
                     'type' => 'attach_image',
                     'heading' => __('Thumbnail Image', 'kek'),
                     'param_name' => 'image_url',
                     'description' => __('Upload or select a thumbnail image from the media gallery.', 'kek'),
+                ),
+                array(
+                    'type' => 'attach_image',
+                    'heading' => __('Background Image', 'kek'),
+                    'param_name' => 'background_image',
+                    'description' => __('Set a background image for the video card.', 'kek'),
                 ),
                 array(
                     'type' => 'textfield',
@@ -57,145 +70,118 @@ class WPBakeryKekVideoCard {
         $atts = shortcode_atts(array(
             'platform' => 'instagram',
             'image_url' => '',
+            'background_image' => '',
             'views_count' => '11200',
             'likes_count' => '2500',
             'comments_count' => '223',
+            'show_icons' => 'no', // Default to showing icons
         ), $atts);
-
+    
         $platform = esc_attr($atts['platform']);
-        $image_url = !empty($atts['image_url']) ? wp_get_attachment_url($atts['image_url']) : '/img/illustrations/comedy-post.webp';
-        $views_count = esc_attr($atts['views_count']);
-        $likes_count = esc_attr($atts['likes_count']);
-        $comments_count = esc_attr($atts['comments_count']);
-
+        $image_url = !empty($atts['image_url']) ? wp_get_attachment_url($atts['image_url']) : '';
+        $background_image = !empty($atts['background_image']) ? wp_get_attachment_url($atts['background_image']) : '';
+    
         // Icons and colors based on platform
         $icon_color = '';
-        $view_icon = $like_icon = $comment_icon = '';
+        $floating_icons = [];
         switch ($platform) {
             case 'youtube':
                 $icon_color = '#FF0000'; // YouTube red
-                $view_icon = 'fas fa-eye';
-                $like_icon = 'fas fa-thumbs-up';
-                $comment_icon = 'fas fa-comments';
+                $floating_icons = [
+                    'fas fa-play', 
+                    'fas fa-heart', 
+                    'fas fa-comment', 
+                    'fas fa-share', 
+                    'fas fa-bell'
+                ];
                 break;
             case 'instagram':
                 $icon_color = '#E1306C'; // Instagram pink
-                $view_icon = 'fas fa-video';
-                $like_icon = 'fas fa-heart';
-                $comment_icon = 'fas fa-comment';
+                $floating_icons = [
+                    'fas fa-camera', 
+                    'fas fa-heart', 
+                    'fas fa-comment', 
+                    'fas fa-share-alt', 
+                    'fas fa-user'
+                ];
                 break;
             case 'tiktok':
                 $icon_color = '#69C9D0'; // TikTok blue
-                $view_icon = 'fas fa-music';
-                $like_icon = 'fas fa-star';
-                $comment_icon = 'fas fa-comment-alt';
+                $floating_icons = [
+                    'fas fa-music', 
+                    'fas fa-heart', 
+                    'fas fa-comment-alt', 
+                    'fas fa-star', 
+                    'fas fa-share'
+                ];
                 break;
         }
-
+    
         ob_start();
         ?>
-        <div class="video-card video-card--<?php echo $platform; ?>">
-           
-            <div class="video-card__metrics">
-            <div class="video-card__metric-cell" >
-            <img loading="lazy" src="<?php echo esc_url($image_url); ?>" alt=" " draggable="false">
-                </div>
-                <div class="video-card__metric-cell" >
-                    <i class="<?php echo $view_icon; ?>"  style="color: <?php echo esc_attr($icon_color); ?>;"></i>
-                    <div class="counter">
-                        <span data-from="0" 
-                              data-to="<?php echo $views_count; ?>" 
-                              data-refresh-interval="20" 
-                              data-speed="2000">
-                              <?php echo $views_count; ?>
-                        </span>
+        <div class="video-card-wrapper" 
+             style="background-image: url('<?php echo esc_url($background_image); ?>');
+              background-size: contain;
+  background-position: center;
+  position: relative;
+  background-repeat: no-repeat;">
+             <?php if ($atts['show_icons'] === 'yes'): ?>
+            <!-- Floating Icons -->
+            <?php foreach ($floating_icons as $index => $icon_class): 
+                        // Define ranges for icon positions to spread them properly
+            $position_ranges = [
+                ['top' => rand(5, 15), 'left' => rand(5, 20)], // Top-left
+                ['top' => rand(5, 15), 'left' => rand(80, 95)], // Top-right
+                ['top' => rand(25, 35), 'left' => rand(15, 35)], // Slightly lower-left
+                ['top' => rand(25, 35), 'left' => rand(65, 85)], // Slightly lower-right
+                ['top' => rand(45, 55), 'left' => rand(40, 60)], // Centered
+            ];
+            $position = $position_ranges[$index % count($position_ranges)];
+            ?>
+                <i class="icon-position dynamic-animation <?php echo $platform; ?>" 
+                style="
+                    --icon-top: <?php echo $position['top']; ?>%; 
+                    --icon-left: <?php echo $position['left']; ?>%; 
+                    --animation-name: bounce; 
+                    --animation-duration: <?php echo rand(2, 5); ?>s; 
+                    color: <?php echo esc_attr($icon_color); ?>;"
+                   title="Icon <?php echo $index + 1; ?>">
+                   <span class="<?php echo esc_attr($icon_class); ?>"></span>
+                </i>
+            <?php endforeach; ?>
+            <?php endif; ?>
+            <!-- Main Video Card -->
+            <div class="video-card video-card--<?php echo $platform; ?>">
+                <div class="video-card__metrics">
+                    <div class="video-card__metric-cell">
+                        <img decoding="async" loading="lazy" 
+                             src="<?php echo esc_url($image_url); ?>" 
+                             alt="Thumbnail" draggable="false">
                     </div>
-                </div>
-                <div class="video-card__metric-cell">
-                    <i class="<?php echo $like_icon; ?>"  style="color: <?php echo esc_attr($icon_color); ?>;"></i>
-                    <div class="counter">
-                        <span data-from="0" 
-                              data-to="<?php echo $likes_count; ?>" 
-                              data-refresh-interval="20" 
-                              data-speed="2000">
-                              <?php echo $likes_count; ?>
-                        </span>
+                    <div class="video-card__metric-cell">
+                        <i class="fas fa-eye" style="color: <?php echo esc_attr($icon_color); ?>;"></i>
+                        <div class="counter"><?php echo esc_attr($atts['views_count']); ?></div>
                     </div>
-                </div>
-                <div class="video-card__metric-cell" >
-                    <i class="<?php echo $comment_icon; ?>" style="color: <?php echo esc_attr($icon_color); ?>;"></i>
-                    <div class="counter">
-                        <span data-from="0" 
-                              data-to="<?php echo $comments_count; ?>" 
-                              data-refresh-interval="20" 
-                              data-speed="2000">
-                              <?php echo $comments_count; ?>
-                        </span>
+                    <div class="video-card__metric-cell">
+                        <i class="fas fa-heart" style="color: <?php echo esc_attr($icon_color); ?>;"></i>
+                        <div class="counter"><?php echo esc_attr($atts['likes_count']); ?></div>
+                    </div>
+                    <div class="video-card__metric-cell">
+                        <i class="fas fa-comment-alt" style="color: <?php echo esc_attr($icon_color); ?>;"></i>
+                        <div class="counter"><?php echo esc_attr($atts['comments_count']); ?></div>
                     </div>
                 </div>
             </div>
         </div>
-         <style>
-           
-.video-card {
-  align-items: center;
-  background: #fff;
-  border-radius: 10px;
-  box-shadow: 0px 1px 8px 0px rgba(0, 0, 0, 0.4);
-  display: flex;
-  flex-direction: column;
-
-  padding: 12px;
-  width: 400px;
-  transition: all 0.7s ease-out;
-  animation: float 6s ease-in-out infinite;
-}
-.video-card__metric-cell i{
-    font-size: 24px;
-}
-.video-card__metric-cell .counter {
-    color: #000 !important;
-  font-size: 16px ;
-  font-weight: normal;
-}
-.video-card__metrics > img {
-  border-radius: 6px;
-  flex-shrink: 0;
-  height: 80px;
-  width: 80px;
-}
-
-.video-card__metrics {
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  flex-grow: 1;
-  justify-content: center;
-  padding-top: 3px;
-}
-
-.video-card__metric-cell {
-    align-items: center;
-  border-right: 1px solid #eee;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100px;
-}
-
-.video-card__metric-cell:last-child {
-  border-right: 0;
-}
-.video-card__metric-cell p {
-  font-size: inherit;
-  padding-top: 5px;
-}
-
-
+        <style>
+        
         </style>
         <?php
         return ob_get_clean();
     }
+    
+    
+    
 }
 
 // Instantiate the class
