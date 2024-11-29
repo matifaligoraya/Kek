0d6c6bbd1b14b1a3e41701af3a3a111ec8e3b07f
M ATIF ALI <matifaligoraya@gmail.com>
2024-11-30 04:08:57 +0500

    testimonial

--- inc/init.php
+++ inc/init.php
@@ -21,7 +21,7 @@ include kek_DIR.'inc/functions/scripts.php';
 include kek_DIR.'inc/functions/plugins.php';
 include kek_DIR.'inc/functions/functions.php';
 include kek_DIR.'inc/functions/hooks.php';
-require kek_DIR.'inc/functions/testimonials.php';
+# require kek_DIR.'inc/functions/testimonials.php';
 require kek_DIR.'inc/functions/woocommerce.php';
 
 /**
