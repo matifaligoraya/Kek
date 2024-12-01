d7cedfe6659003ea346b934719e88503800e75f4
M ATIF ALI <matifaligoraya@gmail.com>
2024-12-01 23:28:30 +0500

    Footer - updated

--- partials/footer-elements.php
+++ partials/footer-elements.php
@@ -19,16 +19,27 @@ if (is_active_sidebar( 'footerfull' )): ?>
 
         <div class="row">
 
-            <div class="col text-center">
+            <div class="col text-center footer-back">
 
-                <footer class="site-footer" id="colophon">
-
+                <footer class="site-footer " id="colophon">
+            
                     <div class="site-info">
 
                         <?php picostrap_site_info(); ?>
 
                     </div><!-- .site-info -->
-
+                    <ul class="circles">
+                    <li><i class="fab fa-youtube"></i></li>
+        <li><i class="fab fa-tiktok"></i></li>
+        <li><i class="fab fa-instagram"></i></li>
+        <li><i class="far fa-comment-alt"></i></li>
+        <li><i class="far fa-thumbs-up"></i></li>
+        <li><i class="fas fa-share-alt"></i></li>
+        <li><i class="fab fa-facebook-f"></i></li>
+        <li><i class="fab fa-twitter"></i></li>
+        <li><i class="fas fa-heart"></i></li>
+        <li><i class="fab fa-linkedin-in"></i></li>
+            </ul>
                 </footer><!-- #colophon -->
 
             </div><!--col end -->
