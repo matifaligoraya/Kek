cfcaf71253ed7001b90fc28278ab97580be0978a
M ATIF ALI <matifaligoraya@gmail.com>
2024-12-02 17:21:07 +0500

    Atif - footer change

--- partials/footer-elements.php
+++ partials/footer-elements.php
@@ -1,34 +1,14 @@
 <?php
 
 if (is_active_sidebar( 'footerfull' )): ?>
-    <div class="wrapper bg-light mt-5 py-5" id="wrapper-footer-widgets">
+    <div class="wrapper bg-light mt-5 py-5 footer-back" id="wrapper-footer-widgets">
         
         <div class="container mb-5">
             
             <div class="row">
                 <?php dynamic_sidebar( 'footerfull' ); ?>
             </div>
-
-        </div>
-    </div>
-<?php endif ?>
-
-
-<div class="wrapper py-3" id="wrapper-footer-colophon">
-    <div class="container-fluid">
-
-        <div class="row">
-
-            <div class="col text-center footer-back">
-
-                <footer class="site-footer " id="colophon">
-            
-                    <div class="site-info">
-
-                        <?php picostrap_site_info(); ?>
-
-                    </div><!-- .site-info -->
-                    <ul class="circles">
+            <ul class="circles">
                     <li><i class="fab fa-youtube"></i></li>
         <li><i class="fab fa-tiktok"></i></li>
         <li><i class="fab fa-instagram"></i></li>
@@ -40,6 +20,26 @@ if (is_active_sidebar( 'footerfull' )): ?>
         <li><i class="fas fa-heart"></i></li>
         <li><i class="fab fa-linkedin-in"></i></li>
             </ul>
+        </div>
+    </div>
+<?php endif ?>
+
+
+<div class="wrapper py-3" id="wrapper-footer-colophon">
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col text-center ">
+
+                <footer class="site-footer " id="colophon">
+            
+                    <div class="site-info">
+
+                        <?php picostrap_site_info(); ?>
+
+                    </div><!-- .site-info -->
+                    
                 </footer><!-- #colophon -->
 
             </div><!--col end -->
